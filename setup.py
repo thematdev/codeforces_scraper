d3b3e7abbc7b04619b6c74a8351552f670575ae6
thematdev <thematdev@thematdev.org>
2024-04-03 21:56:31 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
         name='codeforces-scraper',
-        version='0.3.1',
+        version='0.3.2',
         author='thematdev',
         author_email='thematdev@thematdev.org',
         description='Utility to do actions on codeforces',
